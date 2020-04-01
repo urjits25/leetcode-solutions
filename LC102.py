2179286a4418eebb7993cef3a0eeaf56905c3036
Urjit Sardesai <urjit.s25@gmail.com>
2020-04-01 15:39:08 -0400

    Binary Tree Level Order Traversal

--- LC102.py
+++ LC102.py
@@ -12,23 +12,24 @@ Given a binary tree, return the level order traversal of its nodes' values. (ie,
 
 '''
 
-from collections import deque
-
 def levelOrder(root: TreeNode) -> List[List[int]]:
     if not root:
         return []
-
-    ans, level = [], [root]
-
-    while level:
     
-        ans.append([node.val for node in level if node])
- 
-        temp = []
-        for node in level:
-            if node:
-                temp.extend([node.left, node.right])
-        
-        level = [leaf for leaf in temp if leaf]
+    ans, level = [], [root]
+    while level:
+        ans.append([l.val for l in level])                      # ---- Add ListNode vals from previous levels
+
+        new = []
+        for n in level:                                         # ---- Add children of current level ListNodes to next level
+            if n.left:
+                new.append(n.left)
+            if n.right:
+                new.append(n.right)
+
+        if level is not new:                                    # ---- Exit if we reach leaf nodes
+            level = new
+        else:
+            level = []
 
     return ans
