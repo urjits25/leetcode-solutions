1c019b9443b45ddeb90a9f71ddd9ccbb2004b084
Urjit Sardesai <urjit.s25@gmail.com>
2020-04-03 15:10:13 -0400

    1-bit and 2-bit characters

---
+++ LC717.py
@@ -0,0 +1,22 @@
+'''
+
+1-BIT and 2-BIT CHARACTERS
+
+We have two special characters. The first character can be represented by one bit 0. 
+The second character can be represented by two bits (10 or 11).
+Now given a string represented by several bits. Return whether the last character must be a one-bit character or not. 
+The given string will always end with a zero.
+
+'''
+
+def isOneBitCharacter(self, bits: List[int]) -> bool:
+    if not bits:
+        return True
+    i = 0
+    while i < len(bits)-1:
+        if bits[i] is 1:
+            i += 2
+        else:
+            i += 1
+
+    return i == len(bits)-1
